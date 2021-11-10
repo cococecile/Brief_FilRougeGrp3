5b424cce64610559550a5c0ca6712d5de65173a0
Céccile <simoncecilepro@outlook.fr>
2021-11-10 12:54:33 +0100

    Crea Home/list/employe/xyz

--- gestionpersonnel/frontend/src/app/app.module.ts
+++ gestionpersonnel/frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { XyzComponent } from './xyz/xyz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTutorialComponent,
+    TutorialDetailsComponent,
+    TutorialsListComponent,
+    XyzComponent
   ],
   imports: [
     BrowserModule,
