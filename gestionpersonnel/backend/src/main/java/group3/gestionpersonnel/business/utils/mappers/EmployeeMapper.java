eb813695a4eac4cdee7d7447d9387a0254c4a22a
Alex <alexandraamyhall@gmail.com>
2021-11-10 14:56:38 +0100

    join columns correction

--- gestionpersonnel/backend/src/main/java/group3/gestionpersonnel/business/utils/mappers/EmployeeMapper.java
+++ gestionpersonnel/backend/src/main/java/group3/gestionpersonnel/business/utils/mappers/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package group3.gestionpersonnel.business.utils.mappers;
 
+import org.modelmapper.ModelMapper;
+
 import group3.gestionpersonnel.business.services.interfaces.EmployeeService;
 import group3.gestionpersonnel.persistence.entitties.EmployeeDo;
 import group3.gestionpersonnel.presentation.model.EmployeeDto;
@@ -7,6 +9,7 @@ import group3.gestionpersonnel.presentation.model.EmployeeDto;
 public class EmployeeMapper {
 	
 	private EmployeeService employeeService;
+	ModelMapper modelMapper = new ModelMapper();
 	
 	private EmployeeDto convertToDto(EmployeeDo employeeDo) {
 	    EmployeeDto employeeDto = modelMapper.map(employeeDo, EmployeeDto.class);
