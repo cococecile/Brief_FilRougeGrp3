2ba3feb453a4950248e05ad4adc6f9c91cdebb11
Alex <alexandraamyhall@gmail.com>
2021-11-10 14:34:51 +0100

    fichiers txt enlevés

--- gestionpersonnel/backend/src/main/java/group3/gestionpersonnel/business/utils/mappers/EmployeeMapper.java
+++ gestionpersonnel/backend/src/main/java/group3/gestionpersonnel/business/utils/mappers/EmployeeMapper.java
@@ -1,5 +1,27 @@
 package group3.gestionpersonnel.business.utils.mappers;
 
+import group3.gestionpersonnel.business.services.interfaces.EmployeeService;
+import group3.gestionpersonnel.persistence.entitties.EmployeeDo;
+import group3.gestionpersonnel.presentation.model.EmployeeDto;
+
 public class EmployeeMapper {
+	
+	private EmployeeService employeeService;
+	
+	private EmployeeDto convertToDto(EmployeeDo employeeDo) {
+	    EmployeeDto employeeDto = modelMapper.map(employeeDo, EmployeeDto.class);
+	    return employeeDto;
+	}
+	
+	private EmployeeDo convertToEntity(EmployeeDto employeeDto) {
+	    EmployeeDo employeeDo = modelMapper.map(employeeDto, EmployeeDo.class);
+	 
+	    if (employeeDto.getEmployeeId() != null) {
+	        EmployeeDo oldEmployeeDo = employeeService.getById(employeeDto.getEmployeeId());
+	        employeeDo.setRedditID(oldEmployeeDo.getRedditID());
+	        employeeDo.setSent(oldEmployeeDo.isSent());
+	    }
+	    return employeeDo;
+	}
 
 }
