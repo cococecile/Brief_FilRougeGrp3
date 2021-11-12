1e30399ab0409a2187008a8cb5e936359f8b4705
Céccile <simoncecilepro@outlook.fr>
2021-11-12 21:47:55 +0100

    components

--- gestionpersonnel/backend/src/main/java/group3/gestionpersonnel/business/services/interfaces/EmployeeService.java
+++
@@ -1,7 +0,0 @@
-package group3.gestionpersonnel.business.services.interfaces;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeService extends JpaRepository {
-
-}
