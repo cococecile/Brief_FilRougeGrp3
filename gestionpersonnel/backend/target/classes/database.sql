80a706f0c80b14e88bf046c01b06a87cef5d9378
Lemaire Justine <lemairejustine01@gmail.com>
2021-11-15 16:21:59 +0100

    CREATE NEW BRANCH <feat-JustineFront>

--- gestionpersonnel/backend/target/classes/database.sql
+++ gestionpersonnel/backend/target/classes/database.sql
@@ -1,11 +1,13 @@
 -- Database: gestion_de_personnel
 
-DROP DATABASE gestion_de_personnel;
+-- DROP DATABASE gestion_de_personnel;
 
 CREATE DATABASE gestion_de_personnel
     WITH 
-    OWNER = postgres
+    OWNER = directeur
     ENCODING = 'UTF8'
+    LC_COLLATE = 'French_France.1252'
+    LC_CTYPE = 'French_France.1252'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
@@ -15,6 +17,14 @@ COMMENT ON DATABASE gestion_de_personnel
     -- Role: directeur
 -- DROP ROLE directeur;
 
-CREATE USER backend WITH PASSWORD 'password';
-ALTER DEFAULT PRIVILEGES 
-GRANT INSERT, SELECT, UPDATE, DELETE ON TABLES TO backend;
+CREATE ROLE directeur WITH
+  LOGIN
+  SUPERUSER
+  INHERIT
+  CREATEDB
+  CREATEROLE
+  NOREPLICATION
+  VALID UNTIL '2021-11-10 09:57:11+01';
+
+COMMENT ON ROLE directeur IS 'chef d''entreprise qui a la main sur la BDD 
+Peut tout faire sauf créer des backups ';
