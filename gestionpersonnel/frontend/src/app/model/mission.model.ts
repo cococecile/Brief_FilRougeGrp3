0ddaa40cf5e93c4f839c416296fd076aa4b40e4a
Lemaire Justine <lemairejustine01@gmail.com>
2021-11-16 22:28:32 +0100

    MODELS DEPARTMENT + MISSION

--- gestionpersonnel/frontend/src/app/model/mission.model.ts
+++ gestionpersonnel/frontend/src/app/model/mission.model.ts
@@ -1,5 +1,10 @@
-import { Mission } from "./mission.model";
-
-export class Mission {
-   //
-  }
+ export class Mission{
+   //Reprend les éléments de la class//
+      missionMissionId?: number;
+      missionMissionStartDate?:any;
+      missionMissionEndDate?:any;
+      missionMissionDescription?: string;
+      missionMissionType?: unknown;
+      missionMissionAssignedTo: string;
+      missionMissionIssuedBy?: string;
+     }
