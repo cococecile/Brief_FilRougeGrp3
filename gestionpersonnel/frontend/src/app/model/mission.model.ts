178f613e2a7cc6d4c2a5b2a2269c718815e977de
Lemaire Justine <lemairejustine01@gmail.com>
2021-11-18 12:06:29 +0100

    MISSION MODEL OK

--- gestionpersonnel/frontend/src/app/model/mission.model.ts
+++ gestionpersonnel/frontend/src/app/model/mission.model.ts
@@ -1,3 +1,13 @@
+import { Employee } from "./employee.model";
+
 export class Mission {
- 
+    missionId?: any;
+    missionName?: string;
+    missionDescription?: string;
+    missionStartDate?: string;
+    missionEndDate?: string;
+    missionType?: string;
+    missionAssignedTo?: Employee;
+
+
   }
