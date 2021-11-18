16ae7df7f57a28a145d6db69f083dae15d2a714c
Céccile <simoncecilepro@outlook.fr>
2021-11-18 14:26:04 +0100

    detailmanagerok

--- gestionpersonnel/frontend/src/app/components/manager/manager-detail/manager-detail.component.ts
+++ gestionpersonnel/frontend/src/app/components/manager/manager-detail/manager-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Department } from 'src/app/model/department.model';
+import { Employee } from 'src/app/model/employee.model';
+import { Manager } from 'src/app/model/manager.model';
+import { ManagerService } from 'src/app/services/manager.service';
 
 @Component({
   selector: 'app-manager-detail',
@@ -7,9 +11,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManagerDetailComponent implements OnInit {
 
-  constructor() { }
+   
+  selected?: Manager;
+  currentManager=null;
+  currentIndex: number = -1;
 
-  ngOnInit(): void {
+  managers:[];
+  managerFirstName: string;
+  managerLastName: string;
+  managerEmail: string;
+  managerDepartment: Department;
+
+  department:[];
+  departmentName: string;
+
+  managedEmployee: Employee;
+  employee: [];
+  employeeFirsName: string;
+
+
+
+  constructor(private employeeService: ManagerService) {
   }
 
-}
+  ngOnInit(): void {
+     this.retrieveManagers();
+      }
+    
+      retrieveManagers(): void {
+       this.employeeService.getAll()
+       .subscribe(
+         data => {
+           this.managers = data;
+           console.log(data);
+         },
+         error => {
+           console.log(error);
+         });
+      }
+    
+      refreshList(): void {
+        this.refreshList();
+        this.currentManager = null;
+        this.currentIndex = -1;
+      }
+    
+      setActiveManager(employee,index): void {
+        this.currentManager = employee;
+        this.currentIndex = index;
+      }
+    
+      delete(): void {
+        if (!this.selected) {
+          return;
+        }
+    }
+    
+    }
--- gestionpersonnel/frontend/src/app/model/department.model.ts
+++ gestionpersonnel/frontend/src/app/model/department.model.ts
@@ -2,6 +2,7 @@ import { Employee } from "./employee.model";
 import { Manager } from "./manager.model";
 import { Mission } from "./mission.model";
 
+
 export class Department {
   
    departmentId?: any;
